7bebff52c560c745a9aa9a8346ffc5dc54ed0e61
nzawirski <nikon96@gmail.com>
2020-03-24 18:51:27 +0100

    Pasted doc from my BENG

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ const port = config.port;
 app.use(express.json());
 app.use(cors());
 
+// doc
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./swagger/swagger.yaml');
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 // db
 db.connect().catch((e)=>console.error('Error connecting to database ' + e))
 
 // api
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello world!</h1>`);
+    res.send(`<h1>Hello, Go to <a href='/api-doc'>/api-doc</a> route to see documentation</h1>`);
 });
 app.use('/api/users', require('./controller/user'))
 app.use('/api/me', require('./controller/me'))
